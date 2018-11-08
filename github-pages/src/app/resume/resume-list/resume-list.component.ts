a8059a51500c89649d2dd25f9d2d626d7c2240c9
sarahzhang <zhang.sar@husky.neu.edu>
2018-11-08 14:18:43 -0500

    resume module

---
+++ github-pages/src/app/resume/resume-list/resume-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resume-list',
+  templateUrl: './resume-list.component.html',
+  styleUrls: ['./resume-list.component.css']
+})
+export class ResumeListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
