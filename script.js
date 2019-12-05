546d6ed50161b21d8324b892b41b4b5a8950c64c
Sarah Zhang <zhang.sar@husky.neu.edu>
2019-12-04 14:48:06 -1000

    window click event, window placement, etc

--- script.js
+++ script.js
@@ -15,10 +15,11 @@ for (let i = 0; i < icons.length; i++) {
 }
 
 // close window upon button click
-const windowBtns = document.querySelectorAll('button.title-bar__icon');
+const windowBtns = document.querySelectorAll('.window button');
 for (let i = 0; i < windowBtns.length; i++) {
   let btn = windowBtns[i];
   btn.addEventListener('click', event => {
+    event.stopPropagation();
     let window = btn.closest('.window');
     window.classList.remove('active');
   });
@@ -29,15 +30,20 @@ let desktop = document.querySelector('.desktop');
 const windows = document.querySelectorAll("[id$='window']");
 
 for (let i = 0; i < windows.length; i++) {
-  dragElement(windows[i]);
+  const win = windows[i];
+  dragElement(win);
+  win.addEventListener('click', event => {
+    event.stopPropagation();
+    win.style.zIndex = winZIndex++;
+  });
 }
 
 function dragElement(win) {
   let titleBar = win.firstElementChild;
   // register touch listeners
-  titleBar.addEventListener("touchstart", dragStart, false);
+  titleBar.addEventListener("touchstart", dragStart, {passive: true});
   document.addEventListener("touchend", dragEnd, false);
-  document.addEventListener("touchmove", drag, false);
+  document.addEventListener("touchmove", drag, {passive: true});
   // register mouse listeners
   titleBar.addEventListener("mousedown", dragStart, false);
   document.addEventListener("mouseup", dragEnd, false);
@@ -50,29 +56,26 @@ function dragElement(win) {
 
   function dragStart(e) {
     e = e || window.event;
-    e.preventDefault();
+    // e.preventDefault();
     if (e.type === "touchstart") {
       initX = e.touches[0].clientX - xOffset;
       initY = e.touches[0].clientY - yOffset;
       isDraggable = true;
-      win.style.zIndex = winZIndex++;
     } else {
       initX = e.clientX - xOffset;
       initY = e.clientY - yOffset;
       isDraggable = true;
-      win.style.zIndex = winZIndex++;
     }
   }
   
-  function dragEnd(e) {
+  function dragEnd() {
     isDraggable = false;
   }
   
   function drag(e) {
+    e = e || window.event;
+    // e.preventDefault();
     if (isDraggable) {
-      e = e || window.event;
-      e.preventDefault();
-  
       if (e.type === "touchmove") {
         currentX = initX - e.touches[0].clientX;
         currentY = initY - e.touches[0].clientY;
@@ -99,9 +102,10 @@ function dragElement(win) {
     let winOffsetLeft = win.offsetLeft - currentX;
     if (winOffsetLeft < 0) {
       winOffsetLeft = 0;
-    } else if (winOffsetLeft + win.offsetWidth > parent.outerWidth) {
-      winOffsetLeft = parent.outerWidth - win.offsetWidth;
     }
+    // } else if (winOffsetLeft + win.offsetWidth > parent.outerWidth) {
+    //   winOffsetLeft = parent.outerWidth - win.offsetWidth;
+    // }
     win.style.left = winOffsetLeft + "px";
   }
 }
