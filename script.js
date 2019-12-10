4d30f8354cc6774f0c608fba9cb9d0b64e3a423f
sazhang <zhang.sar@husky.neu.edu>
2019-12-09 23:45:07 -0800

    modified mutation observer

--- script.js
+++ script.js
@@ -5,22 +5,22 @@ const icons = document.querySelectorAll("[id$='icon']");
 for (let i = 0; i < icons.length; i++) {
   let icon = icons[i];
   const id = icon.id;
-  const idx = icon.id.indexOf('-');
+  const idx = icon.id.indexOf("-");
   let window = document.querySelector(`#${id.substr(0, idx)}-window`);
-  icon.addEventListener('click', event => {
-    window.classList.add('active');
+  icon.addEventListener("click", event => {
+    window.classList.add("active");
     window.style.zIndex = winZIndex++;
   });
 }
 
 // close window upon button click
-const windowBtns = document.querySelectorAll('.window button');
+const windowBtns = document.querySelectorAll(".window button");
 for (let i = 0; i < windowBtns.length; i++) {
   let btn = windowBtns[i];
-  btn.addEventListener('click', event => {
+  btn.addEventListener("click", event => {
     event.stopPropagation();
-    let window = btn.closest('.window');
-    window.classList.remove('active');
+    let window = btn.closest(".window");
+    window.classList.remove("active");
   });
 }
 
@@ -30,7 +30,7 @@ const windows = document.querySelectorAll("[id$='window']");
 for (let i = 0; i < windows.length; i++) {
   const win = windows[i];
   dragElement(win);
-  win.addEventListener('click', event => {
+  win.addEventListener("click", event => {
     event.stopPropagation();
     win.style.zIndex = winZIndex++;
   });
@@ -38,21 +38,23 @@ for (let i = 0; i < windows.length; i++) {
 
 function dragElement(win) {
   let titleBar = win.firstElementChild;
-  titleBar.addEventListener("touchstart", dragStart, {passive: true});
+  titleBar.addEventListener("touchstart", dragStart, { passive: true });
   document.addEventListener("touchend", dragEnd, false);
-  document.addEventListener("touchmove", drag, {passive: true});
+  document.addEventListener("touchmove", drag, { passive: true });
   titleBar.addEventListener("mousedown", dragStart, false);
   document.addEventListener("mouseup", dragEnd, false);
   document.addEventListener("mousemove", drag, false);
 
   let isDraggable = false;
-  let currentX; let currentY; let initX; let initY;
+  let currentX;
+  let currentY;
+  let initX;
+  let initY;
   let xOffset = 0;
   let yOffset = 0;
 
   function dragStart(e) {
     e = e || window.event;
-    // e.preventDefault();
     if (e.type === "touchstart") {
       initX = e.touches[0].clientX - xOffset;
       initY = e.touches[0].clientY - yOffset;
@@ -63,14 +65,13 @@ function dragElement(win) {
       isDraggable = true;
     }
   }
-  
+
   function dragEnd() {
     isDraggable = false;
   }
-  
+
   function drag(e) {
     e = e || window.event;
-    // e.preventDefault();
     if (isDraggable) {
       if (e.type === "touchmove") {
         currentX = initX - e.touches[0].clientX;
@@ -86,7 +87,7 @@ function dragElement(win) {
       calcNewPosn();
     }
   }
-  
+
   function calcNewPosn() {
     let winOffsetTop = win.offsetTop - currentY;
     if (winOffsetTop < 0) {
@@ -99,32 +100,22 @@ function dragElement(win) {
     if (winOffsetLeft < 0) {
       winOffsetLeft = 0;
     }
-    // } else if (winOffsetLeft + win.offsetWidth > parent.outerWidth) {
-    //   winOffsetLeft = parent.outerWidth - win.offsetWidth;
-    // }
     win.style.left = winOffsetLeft + "px";
   }
 }
 
 // modify font style of twitter timeline
-function injectStyles(win) {
-  let styleLink = document.createElement("link");
-  styleLink.href = "twitter.css"; 
-  styleLink.rel = "stylesheet"; 
-  styleLink.type = "text/css"; 
-  win.firstElementChild.contentDocument.head.appendChild(styleLink);
-}
+const twitterWin = document.querySelector("#twitter-window .window__content");
+const config = { childList: true, subtree: true };
 
-let observer = new MutationObserver(function (mutations, observer) {
-  let twitterWin = document.querySelector("#twitter-window .window__content");
-  if (twitterWin.firstElementChild.id == "twitter-widget-0") {
-    injectStyles(twitterWin);
+const callback = function(mutations, observer) {
+  const twitterFrame = twitterWin.firstElementChild;
+  if (twitterFrame.id == "twitter-widget-0") {
+    twitterFrame.contentWindow.document.body.style.fontFamily = "Source Code Pro,monospace";
     observer.disconnect();
     return;
   }
-});
+};
 
-observer.observe(document, {
-  childList: true,
-  subtree: true
-});
+const observer = new MutationObserver(callback);
+observer.observe(twitterWin, config);
